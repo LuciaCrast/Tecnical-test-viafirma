4027326339c975b0122798a7c7d63a4c045c5436
Lucia Cano <luciacanoras@gmail.com>
2021-11-19 12:35:00 +0100

    Add reset styles at App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Principal } from "../Principal";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Principal />
+    </div>
+  );
+}
+
+export default App;
